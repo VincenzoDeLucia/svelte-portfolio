9f7a2aedea0051b89dd68c840ff944152ecb2ab0
Vincenzo De Lucia <vincenzo.delucia@icloud.com>
2022-03-13 22:01:03 +0100

    Add header w/ navigation, intro and services

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,53 @@
 <script>
-	export let name;
+	
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<header>
+		<div class="logo">
+			<img src="" alt=""/>
+		</div>
+
+		<button class="nav-toggle" aria-label="toggle navigation">
+			<span class="hamburger"></span>
+		</button>
+		<nav class="nav">
+			<ul class="nav__list">
+				<li class="nav__item"><a href="#home">Home</a></li>
+				<li class="nav__item"><a href="#services">My Services</a></li>
+				<li class="nav__item"><a href="#">About me</a></li>
+				<li class="nav__item"><a href="#">My Work</a></li>
+			</ul>
+		</nav>
+	</header>
+
+	<section class="intro" id="home"> 
+		<h1 class="section__title section__title--intro">Hi, I am <strong>Vincenzo De Lucia</strong></h1>
+		<p class="section__subtitle section__subtitle--intro">front-end developer and UX/UI designer</p>
+		<img src="" alt="Vincenzo De Lucia">
+	</section>
+
+	<section class="my-services" id="services">
+		<h2 class="section__title section__title--services">What I do</h2>
+		<div class="services">
+			<div class="service">
+				<h3></h3>
+				<p></p>
+			</div>
+			<div class="service">
+				<h3></h3>
+				<p></p>
+			</div>
+			<div class="service">
+				<h3></h3>
+				<p></p>
+			</div>
+		</div>
+
+		<a href="#work" class="btn">My work</a>
+	</section>
+
+
 </main>
 
 <style>

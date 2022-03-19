ebbe08a9bcad3197c91281eb60df856e35e3cd43
Vincenzo De Lucia <vincenzo.delucia@icloud.com>
2022-03-19 19:13:00 +0100

    Style 'My work' section and footer, add Font Awesome Icons

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -1,18 +1,66 @@
-<footer>
-  <a href="mailto:vincenzo.delucia@icloud.com" class="footer-link"
+<footer class="footer">
+  <a href="mailto:vincenzo.delucia@icloud.com" class="footer__link"
     >vincenzo.delucia@icloud.com</a
   >
-  <ul class="web-prensence">
+  <ul class="web-presence">
     <li class="web-presence__item">
       <a
         href="https://www.linkedin.com/in/vincenzo-de-lucia/"
-        class="web-presence__link">LinkedIn</a
+        class="web-presence__link"
       >
+        <i class="fa-brands fa-linkedin" />
+        LinkedIn
+      </a>
     </li>
     <li class="web-presence__item">
       <a href="https://github.com/VincenzoDeLucia" class="web-presence__link"
-        >GitHub</a
-      >
+        ><i class="fa-brands fa-github" />
+        GitHub
+      </a>
     </li>
   </ul>
 </footer>
+
+<style>
+  .footer {
+    background-color: #111;
+    color: var(--color-accent);
+    text-align: center;
+    padding: 2.5em 0;
+    font-size: var(--font-size-h3);
+  }
+
+  .footer a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  .footer__link {
+    font-weight: var(--font-weight-bold);
+  }
+
+  .footer__link:hover,
+  .web-presence__link:hover {
+    opacity: 0.7;
+  }
+
+  .footer__link:hover {
+    text-decoration: underline;
+  }
+
+  .web-presence {
+    list-style: none;
+    display: flex;
+    justify-content: center;
+    margin: 2em 0;
+    padding: 0;
+  }
+
+  .web-presence__item {
+    margin: 0 0.5em;
+  }
+
+  .web-presence__link {
+    padding: 0.5em;
+  }
+</style>
--- src/MyWork.svelte
+++ src/MyWork.svelte
@@ -1,6 +1,8 @@
 <section class="my-work" id="work">
   <h2 class="section__title">My work</h2>
-  <p class="section__subtitle" />
+  <p class="section__subtitle section__subtitle--work">
+    A selecttion of my works
+  </p>
 
   <div class="portfolio">
     <a href="#" class="portfolio__item">
@@ -8,3 +10,42 @@
     </a>
   </div>
 </section>
+
+<style>
+  .my-work {
+    background-color: var(--color-dark);
+    color: var(--color-light);
+    text-align: center;
+  }
+
+  .section__subtitle--work {
+    color: var(--clr-accent);
+    font-weight: var(--fw-bold);
+    margin-bottom: 1em;
+  }
+
+  .portfolio {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  }
+
+  .portfolio__item {
+    background: var(--clr-accent);
+    overflow: hidden;
+  }
+
+  .portfolio__item:focus {
+    z-index: 2;
+  }
+
+  .portfolio__img {
+    transition: transform 750ms cubic-bezier(0.5, 0, 0.5, 1),
+      opacity 250ms linear;
+  }
+
+  .portfolio__img:hover,
+  .portfolio__item:focus .portfolio__img {
+    transform: scale(1.2);
+    opacity: 0.75;
+  }
+</style>
